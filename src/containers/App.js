b62c9188f742b614e59e8d2d87bcb5241a93836e
Steve940 <smd940@gmail.com>
2018-08-23 12:42:54 -0400

    wireframing still

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Article from '../components/article';
+import Article from '../components/Article';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
         <Article type='shirt'/>
         <Article type='pants'/>
         <Article type='shoes'/>
