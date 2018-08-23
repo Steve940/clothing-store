28dc6306ebfc84754e7f4498d22146465cce8554
Steve940 <smd940@gmail.com>
2018-08-23 12:53:50 -0400

    more wireframing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Article from '../components/Article';
 import Header from './Header';
+import DisplayCase from './DisplayCase';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <Article type='shirt'/>
-        <Article type='pants'/>
-        <Article type='shoes'/>
+        <Scroll>
+          <DisplayCase/>
+        </Scroll>
+
       </div>
     );
   }
--- src/containers/DisplayCase.js
+++ src/containers/DisplayCase.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Article from '../components/Article';
+import './DisplayCase.css';
 
 const DisplayCase = () => {
     return(
-
+        <div className='displayCase'>Display Case
+            <Article type='shirt'/>
+            <Article type='pants'/>
+            <Article type='shoes'/>  
+            <Article type='shirt'/>
+            <Article type='pants'/>
+            <Article type='shoes'/>  
+            <Article type='shirt'/>
+            <Article type='pants'/>
+            <Article type='shoes'/>  
+            <Article type='shirt'/>
+            <Article type='pants'/>
+            <Article type='shoes'/>  
+            <Article type='shirt'/>
+            <Article type='pants'/>
+            <Article type='shoes'/>  
+            <Article type='shirt'/>
+            <Article type='pants'/>
+            <Article type='shoes'/>            
+        </div>
     )
 }
 
