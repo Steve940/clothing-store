84d5528b6d7ae7bf12ab594dbc2caf7a2fe914b2
Steve940 <smd940@gmail.com>
2018-09-04 21:59:39 -0400

    added pictures

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import DisplayCase from './DisplayCase';
-import Scroll from '../components/Scroll';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <Scroll>
-          <DisplayCase/>
-        </Scroll>
-
+        <DisplayCase/>
       </div>
     );
   }
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
 import logo from '../components/images/Logo.png';
+import cart from '../components/images/cart.png'
 
 const Header = () => {
     return(
         <div className='header'>
-        <img src={logo} alt="SDC"/>
-        Header
+            <img src={logo} alt="SDC"/>
+            <p>Free shipping on purchases of $75+</p>
+            <img src={cart} alt="Cart"/>
         </div>
     );
 }
